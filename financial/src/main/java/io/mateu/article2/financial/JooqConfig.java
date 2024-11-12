ff11b7e15e01d20ed3205020883bbb3e0566f4e1
miguelperezcolom <miguelperezcolom@gmail.com>
2024-11-12 18:17:26 +0100

    use jooq with r2dbc for financial module

--- financial/src/main/java/io/mateu/article2/financial/JooqConfig.java
+++ financial/src/main/java/io/mateu/article2/financial/JooqConfig.java
@@ -1,10 +1,15 @@
 package io.mateu.article2.financial;
 
+import io.r2dbc.spi.ConnectionFactory;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
 import org.jooq.conf.RenderNameStyle;
+import org.jooq.impl.DSL;
 import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.r2dbc.connection.TransactionAwareConnectionFactoryProxy;
 
 @Configuration
 class JooqConfig {
@@ -13,4 +18,14 @@ class JooqConfig {
     public DefaultConfigurationCustomizer myJooqConfiguration() {
         return configuration -> configuration.settings().withRenderNameStyle(RenderNameStyle.LOWER);
     }
+
+        @Bean
+        DSLContext dslContext(ConnectionFactory connectionFactory) {
+        var dsl = DSL.using(
+                new TransactionAwareConnectionFactoryProxy(connectionFactory),
+                SQLDialect.MARIADB
+        );
+        dsl.settings().withRenderNameStyle(RenderNameStyle.LOWER);
+            return dsl;
+        }
 }
